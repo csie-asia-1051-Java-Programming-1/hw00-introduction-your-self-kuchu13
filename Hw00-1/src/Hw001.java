814d632ae55d869c5b1106404e3a98c5ba0d6e1f
kuchu13 <105021033@live.asia.edu.tw>
2016-09-12 15:46:54 +0800

    hw00
    hw00

---
+++ Hw00-1/src/Hw001.java
@@ -0,0 +1,14 @@
+
+public class Hw001 {
+
+
+	public static void main(String[] args) {
+
+		
+		System.out.println("���w��");
+		System.out.println("105021033");
+		System.out.println("�Ȭw�j�Ǹ�u�t");
+		
+	}
+
+}
